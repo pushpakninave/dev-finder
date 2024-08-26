55bfbf8041cfeb44d2185479fdd7f3aa4ec570fc
pushpakninave <pushpakninave.jee@gmail.com>
2024-08-26 17:41:49 +0530

    check with this

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,3 @@
-import { withAuth } from "next-auth/middleware";
+export { default } from "next-auth/middleware";
 
-export default withAuth(
-    function middleware(req) {
-        console.log(":: MIDDLEWARE CALLED :: ")
-    },
-    {
-        callbacks: {
-            authorized: ({ token }) => {
-                if (!token) {
-                    console.error("Token is missing in middleware.");
-                } else {
-                    console.log("token present in middleware :: ");
-                }
-                // This callback returns true if the user is authenticated.
-                return !!token;
-            },
-        },
-    });
-
-export const config = {
-    matcher: ["/your-rooms", "/browse", "/edit-room"],
-};
+export const config = { matcher: ["/your-rooms", "/browse", "/edit-room", "/create-room"] };
